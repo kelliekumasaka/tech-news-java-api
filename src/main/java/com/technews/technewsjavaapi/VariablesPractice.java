c5da72aa19bab3dcd0f3d344cd91886a40258977
kelliekumasaka <kelliek3@gmail.com>
2022-01-17 19:49:02 -0800

    variables practice

--- src/main/java/com/technews/technewsjavaapi/VariablesPractice.java
+++ src/main/java/com/technews/technewsjavaapi/VariablesPractice.java
@@ -2,6 +2,11 @@ package com.technews.technewsjavaapi;
 
 public class VariablesPractice {
     public static void main(String[] args){
-
+        int numOne = 4;
+        double numTwo = 5.6;
+        double sum = numOne + numTwo;
+        String str = "dog" + numOne;
+        System.out.println(sum);
+        System.out.println(str);
     }
 }
